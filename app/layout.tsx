78547f3e2673733460c6d18fc0aad1b7e6d3fe44
cybermobile <mattharding22@gmail.com>
2024-09-07 13:53:14 +0200

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Sidebar } from './components/layout/Sidebar'
 import { Header } from './components/layout/Header'
 import { Footer } from './components/layout/Footer'
+import './globals.css'
 
 export default function RootLayout({
   children,
@@ -10,12 +11,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
+      <body className="bg-gray-100 dark:bg-gray-900">
+        <div className="flex h-screen">
           <Sidebar />
-          <div className="flex-1 flex flex-col overflow-hidden">
+          <div className="flex flex-col flex-1 overflow-hidden">
             <Header />
-            <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 dark:bg-gray-800">
+            <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 dark:bg-gray-800 p-6">
               {children}
             </main>
             <Footer />
